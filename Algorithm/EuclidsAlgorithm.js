08606e18dfa7fc0f57d58494af07ed3d80578d0c
minoru nishino <20012166+tenoctillion@users.noreply.github.com>
2017-10-06 13:56:48 +0800

    Create EuclidsAlgorithm.js

---
+++ Algorithm/EuclidsAlgorithm.js
@@ -0,0 +1,27 @@
+function Euclid(a, b){
+    if(b=='0'){
+        return a;
+    } else if(a>b){
+        a = a-b;
+	return Euclid(a, b);
+    } else {
+        b = b-a;
+	return Euclid(a, b);
+    } 
+};
+
+console.log(Euclid(49, 21));  // Find gcd for 49 and 21
+
+// Adopted from wikipedia
+function EuclidWiki(a,b){
+    while(b!==0){
+        if (a > b){
+	    a = a - b;
+	} else {
+	    b = b - a;
+	}    
+    }
+    return a;
+};
+
+console.log(EuclidWiki(49,21)); // Find gcd for 49 and 21
